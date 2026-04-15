aa51c8abe2b5fbd50b0346030b61f1ace281f965
Leisy Sánchez <leisysado@gmail.com>
2026-04-15 13:48:28 -0500

    move Listaproductos to products.js

---
+++ products.js
@@ -0,0 +1,288 @@
+// Array de productos
+export const Listaproductos = [
+  {
+    imagen:
+      "https://frutocuatro.com/wp-content/uploads/2018/05/camiseta-64000-royal-frente.jpg?x81271",
+    categoria: "Camisetas",
+    productos: [
+      {
+        id: 1,
+        nombre: "Camiseta Nike Dry Fit",
+        descripcion:
+          "Camiseta ligera con tecnología Dry Fit que absorbe el sudor, ideal para entrenamientos intensos.",
+        precio: 85000,
+        imagen: "https://picsum.photos/200?1",
+        stock: 15,
+        talla: ["S", "M", "L", "XL"],
+      },
+      {
+        id: 2,
+        nombre: "Camiseta Adidas Training",
+        descripcion:
+          "Camiseta transpirable diseñada para máxima comodidad durante rutinas deportivas.",
+        precio: 90000,
+        imagen: "https://picsum.photos/200?2",
+        stock: 10,
+        talla: ["M", "L"],
+      },
+      {
+        id: 3,
+        nombre: "Camiseta Puma Sport",
+        descripcion:
+          "Camiseta deportiva de ajuste cómodo, perfecta para uso diario o entrenamiento.",
+        precio: 78000,
+        imagen: "https://picsum.photos/200?3",
+        stock: 20,
+        talla: ["S", "M", "L"],
+      },
+      {
+        id: 4,
+        nombre: "Camiseta Reebok Active",
+        descripcion:
+          "Diseño moderno con tela flexible que permite libertad de movimiento.",
+        precio: 82000,
+        imagen: "https://picsum.photos/200?4",
+        stock: 12,
+        talla: ["M", "L", "XL"],
+      },
+      {
+        id: 5,
+        nombre: "Camiseta Under Armour HeatGear",
+        descripcion:
+          "Tecnología HeatGear que mantiene el cuerpo fresco incluso en altas temperaturas.",
+        precio: 95000,
+        imagen: "https://picsum.photos/200?5",
+        stock: 9,
+        talla: ["S", "M", "L"],
+      },
+    ],
+  },
+  {
+    imagen:
+      "https://pilatos21.vtexassets.com/arquivos/ids/1399946-800-800?v=638569333850270000&width=800&height=800&aspect=true",
+    categoria: "Tenis",
+    productos: [
+      {
+        id: 1,
+        nombre: "Camiseta Nike Dry Fit",
+        descripcion:
+          "Camiseta ligera con tecnología Dry Fit que absorbe el sudor, ideal para entrenamientos intensos.",
+        precio: 85000,
+        imagen: "https://picsum.photos/200?1",
+        stock: 15,
+        talla: ["S", "M", "L", "XL"],
+      },
+      {
+        id: 2,
+        nombre: "Camiseta Adidas Training",
+        descripcion:
+          "Camiseta transpirable diseñada para máxima comodidad durante rutinas deportivas.",
+        precio: 90000,
+        imagen: "https://picsum.photos/200?2",
+        stock: 10,
+        talla: ["M", "L"],
+      },
+      {
+        id: 3,
+        nombre: "Camiseta Puma Sport",
+        descripcion:
+          "Camiseta deportiva de ajuste cómodo, perfecta para uso diario o entrenamiento.",
+        precio: 78000,
+        imagen: "https://picsum.photos/200?3",
+        stock: 20,
+        talla: ["S", "M", "L"],
+      },
+      {
+        id: 4,
+        nombre: "Camiseta Reebok Active",
+        descripcion:
+          "Diseño moderno con tela flexible que permite libertad de movimiento.",
+        precio: 82000,
+        imagen: "https://picsum.photos/200?4",
+        stock: 12,
+        talla: ["M", "L", "XL"],
+      },
+      {
+        id: 5,
+        nombre: "Camiseta Under Armour HeatGear",
+        descripcion:
+          "Tecnología HeatGear que mantiene el cuerpo fresco incluso en altas temperaturas.",
+        precio: 95000,
+        imagen: "https://picsum.photos/200?5",
+        stock: 9,
+        talla: ["S", "M", "L"],
+      },
+    ],
+  },
+  {
+    imagen:
+      "https://exitocol.vtexassets.com/arquivos/ids/31512884/conjunto-deportivo-o-sudadera-para-mujer-en-vioto-sin-capota.jpg?v=638978869737130000",
+    categoria: "Conjuntos",
+    productos: [
+      {
+        id: 1,
+        nombre: "Camiseta Nike Dry Fit",
+        descripcion:
+          "Camiseta ligera con tecnología Dry Fit que absorbe el sudor, ideal para entrenamientos intensos.",
+        precio: 85000,
+        imagen: "https://picsum.photos/200?1",
+        stock: 15,
+        talla: ["S", "M", "L", "XL"],
+      },
+      {
+        id: 2,
+        nombre: "Camiseta Adidas Training",
+        descripcion:
+          "Camiseta transpirable diseñada para máxima comodidad durante rutinas deportivas.",
+        precio: 90000,
+        imagen: "https://picsum.photos/200?2",
+        stock: 10,
+        talla: ["M", "L"],
+      },
+      {
+        id: 3,
+        nombre: "Camiseta Puma Sport",
+        descripcion:
+          "Camiseta deportiva de ajuste cómodo, perfecta para uso diario o entrenamiento.",
+        precio: 78000,
+        imagen: "https://picsum.photos/200?3",
+        stock: 20,
+        talla: ["S", "M", "L"],
+      },
+      {
+        id: 4,
+        nombre: "Camiseta Reebok Active",
+        descripcion:
+          "Diseño moderno con tela flexible que permite libertad de movimiento.",
+        precio: 82000,
+        imagen: "https://picsum.photos/200?4",
+        stock: 12,
+        talla: ["M", "L", "XL"],
+      },
+      {
+        id: 5,
+        nombre: "Camiseta Under Armour HeatGear",
+        descripcion:
+          "Tecnología HeatGear que mantiene el cuerpo fresco incluso en altas temperaturas.",
+        precio: 95000,
+        imagen: "https://picsum.photos/200?5",
+        stock: 9,
+        talla: ["S", "M", "L"],
+      },
+    ],
+  },
+  {
+    imagen:
+      "https://http2.mlstatic.com/D_Q_NP_868434-MCO98700523458_112025-O.webp",
+    categoria: "Accesorios",
+    productos: [
+      {
+        id: 1,
+        nombre: "Camiseta Nike Dry Fit",
+        descripcion:
+          "Camiseta ligera con tecnología Dry Fit que absorbe el sudor, ideal para entrenamientos intensos.",
+        precio: 85000,
+        imagen: "https://picsum.photos/200?1",
+        stock: 15,
+        talla: ["S", "M", "L", "XL"],
+      },
+      {
+        id: 2,
+        nombre: "Camiseta Adidas Training",
+        descripcion:
+          "Camiseta transpirable diseñada para máxima comodidad durante rutinas deportivas.",
+        precio: 90000,
+        imagen: "https://picsum.photos/200?2",
+        stock: 10,
+        talla: ["M", "L"],
+      },
+      {
+        id: 3,
+        nombre: "Camiseta Puma Sport",
+        descripcion:
+          "Camiseta deportiva de ajuste cómodo, perfecta para uso diario o entrenamiento.",
+        precio: 78000,
+        imagen: "https://picsum.photos/200?3",
+        stock: 20,
+        talla: ["S", "M", "L"],
+      },
+      {
+        id: 4,
+        nombre: "Camiseta Reebok Active",
+        descripcion:
+          "Diseño moderno con tela flexible que permite libertad de movimiento.",
+        precio: 82000,
+        imagen: "https://picsum.photos/200?4",
+        stock: 12,
+        talla: ["M", "L", "XL"],
+      },
+      {
+        id: 5,
+        nombre: "Camiseta Under Armour HeatGear",
+        descripcion:
+          "Tecnología HeatGear que mantiene el cuerpo fresco incluso en altas temperaturas.",
+        precio: 95000,
+        imagen: "https://picsum.photos/200?5",
+        stock: 9,
+        talla: ["S", "M", "L"],
+      },
+    ],
+  },
+  {
+    imagen:
+      "https://f.fcdn.app/imgs/422ab3/www.globalsports.com.uy/gls/4dc1/original/catalogo/ADJN5459-3283-1/460x460/adidas-short-squadra-25-black-white.jpg",
+    categoria: "Shorts",
+    productos: [
+      {
+        id: 1,
+        nombre: "Camiseta Nike Dry Fit",
+        descripcion:
+          "Camiseta ligera con tecnología Dry Fit que absorbe el sudor, ideal para entrenamientos intensos.",
+        precio: 85000,
+        imagen: "https://picsum.photos/200?1",
+        stock: 15,
+        talla: ["S", "M", "L", "XL"],
+      },
+      {
+        id: 2,
+        nombre: "Camiseta Adidas Training",
+        descripcion:
+          "Camiseta transpirable diseñada para máxima comodidad durante rutinas deportivas.",
+        precio: 90000,
+        imagen: "https://picsum.photos/200?2",
+        stock: 10,
+        talla: ["M", "L"],
+      },
+      {
+        id: 3,
+        nombre: "Camiseta Puma Sport",
+        descripcion:
+          "Camiseta deportiva de ajuste cómodo, perfecta para uso diario o entrenamiento.",
+        precio: 78000,
+        imagen: "https://picsum.photos/200?3",
+        stock: 20,
+        talla: ["S", "M", "L"],
+      },
+      {
+        id: 4,
+        nombre: "Camiseta Reebok Active",
+        descripcion:
+          "Diseño moderno con tela flexible que permite libertad de movimiento.",
+        precio: 82000,
+        imagen: "https://picsum.photos/200?4",
+        stock: 12,
+        talla: ["M", "L", "XL"],
+      },
+      {
+        id: 5,
+        nombre: "Camiseta Under Armour HeatGear",
+        descripcion:
+          "Tecnología HeatGear que mantiene el cuerpo fresco incluso en altas temperaturas.",
+        precio: 95000,
+        imagen: "https://picsum.photos/200?5",
+        stock: 9,
+        talla: ["S", "M", "L"],
+      },
+    ],
+  },
+];
